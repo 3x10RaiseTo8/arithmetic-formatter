06f73c6b8522c6282e77a28d882472ecff7937c3
3x10RaiseTo8 <abhisheks1177@gmail.com>
2021-11-15 21:03:39 +0530

    Beautify the code

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,19 +1,24 @@
 import re
 
-def arithmetic_arranger(problems, answer = False):
+
+def arithmetic_arranger(problems, answer=False):
 
     # Error checking
 
-    if len(problems) < 1: return 'Error: No problem found'
-    if len(problems) > 5: return 'Error: Too many problems.'
+    if len(problems) < 1:
+        return 'Error: No problem found'
+    if len(problems) > 5:
+        return 'Error: Too many problems.'
 
     for item in problems:
 
         matchtest1 = re.fullmatch('\s*.+\s*\+\s*.+\s*', item)
         matchtest2 = re.fullmatch('\s*.+\s*-\s*.+\s*', item)
 
-        if matchtest1 != None or matchtest2 !=  None: pass
-        else: return "Error: Operator must be '+' or '-'."
+        if matchtest1 != None or matchtest2 != None:
+            pass
+        else:
+            return "Error: Operator must be '+' or '-'."
 
         if '+' in item:
             numbers = item.split('+')
@@ -46,8 +51,10 @@ def arithmetic_arranger(problems, answer = False):
         num2 = pieces[1].strip()
         no1 = int(num1)
         no2 = int(num2)
-        if op == '+': ans = str(no1 + no2)
-        else: ans = str(no1 - no2)
+        if op == '+':
+            ans = str(no1 + no2)
+        else:
+            ans = str(no1 - no2)
 
         width = max(len(num1), len(num2))
 
@@ -55,13 +62,16 @@ def arithmetic_arranger(problems, answer = False):
             row1 = num1.rjust(width + 2)
             row2 = op + ' ' + num2.rjust(width)
             row3 = '-' * (width + 2)
-            if answer == True: row4 = ans.rjust(width + 2)
+            if answer == True:
+                row4 = ans.rjust(width + 2)
             firstproblem = False
         else:
             row1 += num1.rjust(width + 6)
             row2 += '    ' + op + ' ' + num2.rjust(width)
             row3 += '    ' + '-' * (width + 2)
-            if answer == True: row4 += ans.rjust(width + 6)
+            if answer == True:
+                row4 += ans.rjust(width + 6)
 
-    if answer == True: return row1 + '\n' + row2 + '\n' + row3 + '\n' + row4
+    if answer == True:
+        return row1 + '\n' + row2 + '\n' + row3 + '\n' + row4
     return row1 + '\n' + row2 + '\n' + row3
